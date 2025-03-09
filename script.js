fc27a6f0f95926255dc922891c63f4dc726a7a1c
beemovier <erick129ferreira@gmail.com>
2025-03-09 09:08:31 -0300

    só não entendi por que não vai

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Selecionando elementos
 const atraso1 = 4000;
-const atraso2 = 2000;
+const atraso2 = 3000;
 
 let mensagemElemento = document.getElementById("mensagem");
 let botao = document.getElementById("meuBotao");
@@ -9,36 +9,36 @@ let botao = document.getElementById("meuBotao");
 if (mensagemElemento && botao) {
   // Função para mudar a mensagem com atraso
   function mudarMensagem() {
-    botao.style.display = "none"; // Oculta o botão
-
     mensagemElemento.textContent = "A vida não precisa ser chata. Desafie-se!";
-
+   botao.style.display = "none"; // Oculta o botão
     // Usando setTimeout para criar um atraso de 3 segundos
     setTimeout(function () {
       mensagemElemento.textContent = "Tudo melhora quando tentamos ser melhores do que nós mesmos.";
       setTimeout(function () {
-        mensagemElemento.textContent = "Dê o melhor de sí";
+        mensagemElemento.textContent = "Dê o melhor de sí, o caminho fácil sempre tem fila";
         setTimeout(function () {
-          mensagemElemento.textContent = "Não seja como eu!";
-          setTimeout(function (){
-            let mensagemParcial = "Não seja uma imitação, mas sim...";
+          mensagemElemento.textContent = "Não tente ser melhor que ninguém. Se inspire!";
+          setTimeout(function () {
+            let mensagemParcial = "Longe de ser uma imitação, mas sim: ";
             let mensagemColorida = document.createElement("span");
-            mensagemColorida.textContent = " a sua melhor versão!";
-            mensagemColorida.style.color = "red";
             mensagemElemento.textContent = mensagemParcial;
             mensagemElemento.appendChild(mensagemColorida);
-
-            // Reiniciando após 4 segundos
-            setTimeout(function() {
-              mensagemElemento.textContent = "Clique no botão abaixo para mudar a mensagem.";
-              mensagemElemento.style.color = ""; // Removendo a cor
-              botao.style.display = "block"; // Exibe o botão novamente
-            }, atraso1);
+            setTimeout(function () {
+              mensagemColorida.textContent = "Polindo a sua melhor versão!";
+              mensagemColorida.style.color = "#0056b3";
+              setTimeout(function () {
+                mensagemElemento.textContent = "Clique no botão abaixo para mudar a mensagem.";
+                mensagemElemento.style.color = ""; // Removendo a cor
+                botao.style.display = "block"; // Exibe o botão novamente
+              }, atraso2);
+            }, atraso2);
           }, atraso1);
         }, atraso1);
       }, atraso1);
-    }, atraso2);
+    }, atraso1);
   }
+
+
 
   // Adicionando evento de clique ao botão
   botao.addEventListener("click", mudarMensagem);
